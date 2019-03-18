11a0293c0afa7921538e9f0bf2b76547804b2f43
piyush vishnoi <piyushvishnoipvsh123@gmail.com>
2019-03-18 12:41:57 +0530

    created sub function of class

--- file1.cpp
+++ file1.cpp
@@ -3,7 +3,7 @@
 class calc
 {
 public:
-int m,n,sum;
+int m,n,sum,minus;
 void addition()
 {
 cout<<"enter no for addition";
@@ -12,9 +12,23 @@ sum=m+n;
 cout<<sum;
 }
 };
+calc::void sub()
+{
+cout<<"enter no to be subtracted";
+cin>>m>>n;
+if(m>n)
+{
+minus=m-n;
+}
+else
+{
+minus=n-m;
+}
+}
 void main()
 {
 calc c;
 c.addition();
+c.sub();
 }
 
